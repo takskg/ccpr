3c6a3222fddd016c180221c49244b043f642aee5
Takahiro Sugaya <takskg@gmail.com>
2019-06-10 02:30:47 +0900

    update:加減算が出来るようにした

--- src/main.c
+++ src/main.c
@@ -7,11 +7,27 @@ int main(int argc, char **argv){
         return 1;
     }
 
+    char* argPtr = argv[1];
     printf(".intel_syntax noprefix\n");
     printf(".global main\n");
     printf("main:\n");
-    printf("  mov rax, %d\n", atoi(argv[1]));
+    printf("  mov rax, %ld\n", strtol(argPtr, &argPtr, 10));
+
+    while(*argPtr){
+        if(*argPtr == '+'){
+            ++argPtr;
+            printf("  add rax, %ld\n", strtol(argPtr, &argPtr, 10));
+            continue;
+        }else if(*argPtr == '-'){
+            ++argPtr;
+            printf("  sub rax, %ld\n", strtol(argPtr, &argPtr, 10));
+            continue;
+        }
+
+        printf("想定してない文字です。コンパイルできません。文字:%c\n", *argPtr);
+        return 1;
+    }
+
     printf("  ret\n");
-    
     return 0;
 }
